eb467a1294fa39c56967f76ede66fb2c1be1cd45
Diego Nadur <diegonadurcrv@gmail.com>
2026-08-19 10:43:02 -0300

    fix: ajusta o encolhimento da logo EM em telas menores (320px)

--- src/components/HEADER/index.tsx
+++ src/components/HEADER/index.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="fixed top-0 z-50 w-full bg-branco drop-shadow-lg">
-      <div className="container mx-auto flex items-center top-0 z-50 px-12 py-4 justify-between">
-        <Link href="/" className="relative h-20 w-20">
-          <Image src="/img/Logo-nome-SemFundo.png" alt="" fill sizes="100px" />
+      <div className="container mx-auto flex items-center top-0 z-50 px-4 md:px-12 py-4 justify-between">
+        <Link href="/" className="relative h-20 w-20 shrink-0">
+          <Image src="/img/Logo-nome-SemFundo.png" alt="" fill sizes="100px" className="object-contain"/>
         </Link>
         <div className="flex gap-4 lg:gap-8 items-center font-semibold">
           <div className="flex items-center gap-4">
