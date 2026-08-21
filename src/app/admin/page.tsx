cf2c00497489c67800aecfb5c97aa90cc8938b0d
Shuropa <d2026006049@unifei.edu.br>
2026-08-21 15:16:30 -0300

    feature: adicionando cards de status de agendamentos

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { ADMIN_STORAGE_KEY } from "@/lib/adminAuth";
 import AdminLogin from "@/components/AdminLogin";
+import { Calendar } from "lucide-react";
 
 export default function AdminPage() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
@@ -30,8 +31,8 @@ export default function AdminPage() {
   }
 
   return (
-    <main className="min-h-screen p-8 bg-gray-50">
-      <div className="max-w-6xl mx-auto">
+    <main className="flex flex-col w-full min-h-screen p-8 bg-branco gap-8">
+      <div className="max-w-6xl container mx-auto">
         <header className="flex justify-between items-center mb-8 pb-4 border-b border-gray-200">
           <div>
             <h1 className="text-3xl font-bold text-primaria">
@@ -58,6 +59,74 @@ export default function AdminPage() {
             gerenciamento.
           </p>
         </div>
+      </div>
+      <div>
+        <section className="flex gap-2 w-full justify-around">
+          <div className="flex flex-col gap-6 bg-white px-6 py-4 w-full rounded-lg drop-shadow-xl">
+            <div className="flex justify-between text-lg">
+              <h3>Hoje</h3>
+              <Calendar
+                size={30}
+                className="px-1 py-1 bg-slate-100 rounded-md"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-4xl">21</h1>
+              <p>
+                <span className="text-green-500">+ 3 hoje</span> consultas
+                agendadas
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-6 bg-white px-6 py-4 w-full rounded-lg drop-shadow-xl">
+            <div className="flex justify-between text-lg">
+              <h3>Hoje</h3>
+              <Calendar
+                size={30}
+                className="px-1 py-1 bg-slate-100 rounded-md"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-4xl">21</h1>
+              <p>
+                <span className="text-red-500">+ 3 hoje</span> consultas
+                agendadas
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-6 bg-white px-6 py-4 w-full rounded-lg drop-shadow-xl">
+            <div className="flex justify-between text-lg">
+              <h3>Hoje</h3>
+              <Calendar
+                size={30}
+                className="px-1 py-1 bg-slate-100 rounded-md"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-4xl">21</h1>
+              <p>
+                <span className="text-green-700">+ 3 hoje</span> consultas
+                agendadas
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-6 bg-white px-4 py-4 w-full rounded-lg drop-shadow-xl">
+            <div className="flex justify-between text-lg">
+              <h3>Hoje</h3>
+              <Calendar
+                size={30}
+                className="px-1 py-1 bg-slate-100 rounded-md"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-4xl">21</h1>
+              <p>
+                <span className="text-green-700">+ 3 hoje</span> consultas
+                agendadas
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
     </main>
   );
